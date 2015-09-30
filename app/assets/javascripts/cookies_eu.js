f8198c56c05248b80bff4721ec2e0fb573758816
williamcoates <william.coates@gmail.com>
2015-09-30 16:12:21 +0100

    Fixed javascript so it always works with turbolinks.

--- app/assets/javascripts/cookies_eu.js
+++ app/assets/javascripts/cookies_eu.js
@@ -1,9 +1,12 @@
 //= require jquery.cookie
 
-$(document).ready( function(){
+installCookies = function () {
   $('.cookies-eu-ok').click(function(e){
-    e.preventDefault();
-    $.cookie('cookie_eu_consented', 'true', { path: '/', expires: 365 });
-    $('.cookies-eu').remove();
-  });
-});
+    e.preventDefault()
+    $.cookie('cookie_eu_consented', 'true', { path: '/', expires: 365 })
+    $('.cookies-eu').remove()
+  })
+}
+
+$(document).ready(installCookies)
+$(document).on("page:load", installCookies)
